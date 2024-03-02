a31634b5140fe412c715683bf946dbd4bc1efded
nguyentruong <nguyentruong17@augustana.edu>
2024-03-02 16:15:11 -0600

    fix: make mqtt work again

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import { defineConfig } from 'vite';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -12,22 +10,8 @@ export default defineConfig({
 			// Node.js global to browser globalThis
 			define: {
 				global: 'globalThis'
-			},
-			// Enable esbuild polyfill plugins
-			plugins: [
-				NodeGlobalsPolyfillPlugin({
-					buffer: true,
-					process: true,
-				}),
-			],
+			}
 		}
-	},
-	build: {
-		rollupOptions: {
-			// Enable rollup polyfills plugin
-			// used during production bundling
-			plugins: [nodePolyfills()],
-		},
 	},
 	resolve: {
 		alias: {
